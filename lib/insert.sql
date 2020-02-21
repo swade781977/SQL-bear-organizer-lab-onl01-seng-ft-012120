bafc7cfc759fd8ef4784b3a86ce5177dd1b35fd3
Chris Wade <swade781977@yahoo.com>
2020-02-21 05:29:29 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Choc
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 4, "male", "brown", "mean", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 7, "female", "black", "nice", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 23,"male", "black", "sleepy", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 9, "Grizzly", "mean", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 5, "brown", "nice", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 10, "black", "hungry", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 3, "grizzly", "mean", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 9, "female", "Grizzly", "mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 5, "male", "brown", "nice", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 10,"female", "black", "hungry", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 3,"male", "grizzly", "mean", 0);
