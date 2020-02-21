81eec21081cb809568d3d6fe3782ab6464fd5b38
Chris Wade <swade781977@yahoo.com>
2020-02-21 05:27:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,15 +11,15 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT MAX(age) bears.name, bears.age FROM bears;"
+  "SELECT MAX(age), bears.name, bears.age FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT MIN(age) bears.name, bears.age FROM bears;"
+  "SELECT MIN(age), bears.name, bears.age FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color) FROM bears;"
+  "SELECT COUNT(MAX(color)) FROM bears;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
@@ -27,5 +27,5 @@ def counts_number_of_bears_with_goofy_temperaments
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = NULL;"
+  "SELECT * FROM bears WHERE name = nil;"
 end
