ec65d3f40f1309b40105e3ee851b12b7b2d24943
Chris Wade <swade781977@yahoo.com>
2020-02-21 06:15:14 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 15, "male", "black", "mean", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Rowdy", 4, "male", "brown", "mean", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Tabitha", 7, "female", "black", "nice", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 23, "black", "sleepy", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 9, "Grizzly", "mean", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 5, "brown", "nice", 0);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 10, "black", "hungry", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(null, 3, "grizzly", "mean", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 23,"male", "black", "sleepy", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Melissa", 9, "female", "Grizzly", "mean", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Grinch", 5, "male", "brown", "nice", 0);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES("Wendy", 10,"female", "black", "hungry", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES(NULL, 3,"male", "grizzly", "mean", 0);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE gender = "F";"
+  "SELECT bears.name, bears.age FROM bears WHERE gender = \"F\";"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -7,25 +7,25 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT bears.name, bears.age FROM bears WHERE alive = 1 SORT BY age ASC;"
+  "SELECT bears.name, bears.age FROM bears WHERE alive = 1 ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE MAX(age);"
+  "SELECT bears.name, MAX(age) FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE MIN(age);"
+  "SELECT bears.name, MIN(age) FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT COUNT(color) FROM bears WHERE MAX(color);"
+  "SELECT MAX(color), COUNT(color) FROM bears WHERE color = \"dark brown\";"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament) FROM bears WHERE temperament = "goofy";"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = \"goofy\";"
 end
 
 def selects_bear_that_killed_Tim
-  "SELECT * FROM bears WHERE name = NULL;"
+  "SELECT * FROM bears WHERE id = 8;"
 end
